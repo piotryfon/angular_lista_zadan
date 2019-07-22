f3edd91f4f75993b8d14fc035fabc23280d2547e
piotryfon <pit.pit@wp.pl>
2019-07-22 21:20:15 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-zadan';
+
+  title = 'lista zadań';
+
+  newTask: string;
+
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.tasksList);
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter(e => e !== task);
+  }
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+    console.log(this.tasksDone);
+  }
 }
